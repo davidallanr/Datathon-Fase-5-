b29658605433a705113f31eb9a85f19d4f007c12
David Allan <89280553+davidallanr@users.noreply.github.com>
2026-03-18 14:22:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 # carregar dados
 df = pd.read_csv("DadosNormalizados-Final.csv")
